d62df5be360951d26408115038bd83a9d2fe7e18
ShonSagoro <ramosproque@gmail.com>
2022-11-27 00:54:53 -0600

    Login and Register Fixed style and fetch

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,40 +1,53 @@
-import { Button, Typography } from "@mui/material";
-import "../card/card.css"
+import { Button, Typography, Box } from "@mui/material";
+import "../card/card.css";
 
-const Card=({title,image,button})=> {
-    return (
-        <>
-         <Card sx={{ display: 'flex' }}>
-         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
+const Card = ({ title, image, button }) => {
+  return (
+    <>
+      <Card sx={{ display: "flex" }}>
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
             <Typography variant="h2">{title}</Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Mac Miller
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
-          <IconButton aria-label="previous">
-            {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
-          </IconButton>
-          <IconButton aria-label="play/pause">
-            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-          </IconButton>
-          <IconButton aria-label="next">
-            {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
-          </IconButton>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              Mac Miller
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+            <IconButton aria-label="previous">
+              {theme.direction === "rtl" ? (
+                <SkipNextIcon />
+              ) : (
+                <SkipPreviousIcon />
+              )}
+            </IconButton>
+            <IconButton aria-label="play/pause">
+              <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+            </IconButton>
+            <IconButton aria-label="next">
+              {theme.direction === "rtl" ? (
+                <SkipPreviousIcon />
+              ) : (
+                <SkipNextIcon />
+              )}
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="/static/images/cards/live-from-space.jpg"
-        alt="Live from space album cover"
-      />
-              
-            
-            <Button  variant="contained" color="primary">{button}</Button>
-        </Card>
-        </>
-    )
-    }
-    export default Card;
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image="/static/images/cards/live-from-space.jpg"
+          alt="Live from space album cover"
+        />
+
+        <Button variant="contained" color="primary">
+          {button}
+        </Button>
+      </Card>
+    </>
+  );
+};
+export default Card;
