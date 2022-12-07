82718704c71b376ad2bc17c783e76b325e5cb36c
ShonSagoro <ramosproque@gmail.com>
2022-12-07 11:22:29 -0600

    SUELTAME

--- src/pages/admi/UploadAdmin/AlbumUPage.jsx
+++ src/pages/admi/UploadAdmin/AlbumUPage.jsx
@@ -3,18 +3,13 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import styles from "./signUp.module.css";
 import {
-  TextField,
   Button,
   Typography,
   InputAdornment,
-  IconButton,
   FormControl,
   FilledInput,
   InputLabel,
 } from "@mui/material";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
 
 function AlbumUPage() {
@@ -61,10 +56,7 @@ function AlbumUPage() {
   return (
     <>
       <div className="container">
-        <div className="img-login">
-          <img src="images/audioos.svg" />
-        </div>
-        <div className="login-content">
+        <div className="content">
           <form onSubmit={handleSubmit}>
             <img src="images/logo.png" />
             <Typography
@@ -73,10 +65,10 @@ function AlbumUPage() {
               fontWeight={500}
               align="center"
             >
-              Registro
+              name
             </Typography>
             <FormControl sx={{ my: 2 }} variant="filled" fullWidth>
-              <InputLabel fullWidth>Email</InputLabel>
+              <InputLabel fullWidth>Name</InputLabel>
               <FilledInput
                 value={values.email}
                 type={"email"}
@@ -89,77 +81,6 @@ function AlbumUPage() {
                 }
               ></FilledInput>
             </FormControl>
-            <FormControl sx={{ my: 2 }} variant="filled" fullWidth>
-              <InputLabel fullWidth>Nombre de usuario</InputLabel>
-              <FilledInput
-                value={values.username}
-                type={"text"}
-                onChange={handleChange("username")}
-                id="input-with-icon-adornment"
-                endAdornment={
-                  <InputAdornment edge="end">
-                    <AccountCircleIcon />
-                  </InputAdornment>
-                }
-              ></FilledInput>
-            </FormControl>
-
-            <FormControl sx={{ my: 2 }} variant="filled" fullWidth>
-              <InputLabel fullWidth>Contraseña</InputLabel>
-              <FilledInput
-                id="filled-adornment-password"
-                type={values.showPassword ? "text" : "password"}
-                value={values.password}
-                onChange={handleChange("password")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <FormControl sx={{ my: 2 }} variant="filled" fullWidth>
-              <InputLabel fullWidth>Confirmar Contraseña</InputLabel>
-              <FilledInput
-                id="filled"
-                type={values.showPasswordConfirm ? "text" : "password"}
-                value={values.passwordConfirm}
-                onChange={handleChange("passwordConfirm")}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPasswordCofirm}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPasswordConfirm ? (
-                        <VisibilityOff />
-                      ) : (
-                        <Visibility />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              />
-            </FormControl>
-            <Button
-              color="secondary"
-              style={{ fontSize: ".75em" }}
-              sx={{ marginLeft: 22.3, marginRight: 0 }}
-              align="left"
-              textSizeSmall
-              onClick={handleClick}
-            >
-              ¿Ya tienes una cuenta? Inicia Sesión
-            </Button>
             <Button
               sx={{ my: 2 }}
               variant="contained"
